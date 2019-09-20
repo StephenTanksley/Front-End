b10e9f58b82882fdb87919bc5c24530ca59c424b
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-09-20 16:22:11 -0500

    Changed some code in the PostForm document.

--- expat-journal-app/src/components/NewPosts/PostForm.jsx
+++ expat-journal-app/src/components/NewPosts/PostForm.jsx
@@ -29,26 +29,12 @@ const PostForm = ({ errors, touched, status }) => {
             {touched.name && errors.name && <p className='error'>{errors.name}</p>}
             <Field type="text" name="name" placeholder="Name" /><br /><br />
 
-            {touched.email && errors.email && <p className='error'>{errors.email}</p>}
-            <Field type="text" name="email" placeholder="Email" /><br /><br />
+            {touched.location && errors.location && <p className='error'>{errors.location}</p>}
+            <Field type="text" name="location" placeholder="Location (country, city)" /><br /><br />
 
-            {touched.password && errors.password && <p className='error'>{errors.password}</p>}
-            <Field type="text" name="password" placeholder="Password" /><br /><br />
+            {touched.date && errors.date && <p className='error'>{errors.date}</p>}
+            <Field type="text" name="date" placeholder="Date" /><br /><br />
 
-            {touched.role && errors.role && <p className='error'>{errors.role}</p>}
-            <Field component="select" name="role" placeholder="Role">
-              <option value="" disabled>Select Role: </option>
-              <option value="developer">Developer</option>
-              <option value="legal">Legal</option>
-              <option value="ux-designer">UX Designer</option>
-            </Field><br /><br />
-
-
-            {touched.terms && errors.terms && <p className='error'>{errors.terms}</p>}
-            <label>  
-              <span>Do you agree to the Terms and Conditions?</span>
-              <Field type="checkbox" name = "terms" />
-            </label><br /><br />
             <button type="submit" name="submit">Submit</button>
             </Form>
           </FormContainer>
