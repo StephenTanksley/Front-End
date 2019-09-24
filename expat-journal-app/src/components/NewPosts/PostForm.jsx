0a13f6a8ea00aab8d273f583bd16c31722dcef7e
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-09-24 18:36:16 -0500

    Tim and I are rebuilding the PostForm component using Don's tutorial video to learn how to make CRUD functionality.

--- expat-journal-app/src/components/NewPosts/PostForm.jsx
+++ expat-journal-app/src/components/NewPosts/PostForm.jsx
@@ -6,14 +6,32 @@ import { Container } from '../Styles/Styles'
 //This component accepts user input. 
 
 const PostForm = () => {
+  const [formValues, setFormValues] = useState({
+    id: null,
+    name: "",
+    location: "",
+    date: "",
+    description: ""
+  })
 
+  function handleChange({target: {name, value}}) {
+    setFormValues({...formValues, [name]: value})
+  }
+
+  function handleSubmit(e) { 
+    e.preventDefault();
+    console.log(formValues);
+  }
 
   return(
       <div className='new-entry'>
         <Container>
-        <h1>Hello</h1>
+          <form onSubmit={handleSubmit}>
+            <label>Name: </label>
+            <input name="name" value={formValues.name} onChange={handleChange} />
 
 
+          </form>
         </Container>
       </div>
   )}
