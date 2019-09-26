6eec5b91ba49e18cda94d6499dce4467ecdcb145
timislearning <tim.mitzen@gmail.com>
2019-09-26 06:37:41 -0700

    Add switch into react import

--- expat-journal-app/src/components/Dashboard/Dashboard.jsx
+++ expat-journal-app/src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {Route} from 'react-router-dom'
+import {Route , Switch} from 'react-router-dom'
 import { Container, GridView } from '../Styles/Styles'
 import NewJournal from '../NewPosts/NewJournal'
 import JournalForm from '../NewPosts/JournalForm' 
